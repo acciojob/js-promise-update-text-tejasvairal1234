ff12811189667764cdeb80f7ce82cfeb22350fb9
tejasvairal1234 <tejasvairal7066@gmail.com>
2025-12-31 18:53:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-//your JS code here. If required.
+// your JS code here
+function getMessage() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000); // 1 second
+  });
+}
+
+const output = document.querySelector("#output");
+
+getMessage().then((message) => {
+  output.textContent = message;
+});
